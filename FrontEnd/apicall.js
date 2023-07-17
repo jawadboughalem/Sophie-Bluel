fe297bcf4005022f40802be0336655de1617b9cb
jawadboughalem <jawadboughalem91@gmail.com>
2023-07-17 17:12:02 +0200

    Ajout de travail

--- FrontEnd/apicall.js
+++ FrontEnd/apicall.js
@@ -66,6 +66,7 @@ function loadCategories() {
     .then(categoriesData => {
       categories = ['TOUS', ...categoriesData.map(category => category.name)];
       displayCategories();
+      fillCategorySelect();
     });
 }
 // Fonction pour afficher les catégories
@@ -75,6 +76,18 @@ function displayCategories() {
   categories.forEach(category => {
     const button = createCategoryButton(category);
     categoryContainer.appendChild(button);
+  });
+}
+
+// Fonction pour remplir le select du formulaire avec les catégories
+function fillCategorySelect() {
+  const selectCategory = document.getElementById('select-category');
+  const selectableCategories = categories.filter(category => category !== 'TOUS'); // Filtrer 'TOUS' de la liste
+  selectableCategories.forEach(category => {
+    const option = document.createElement('option');
+    option.value = category;
+    option.textContent = category;
+    selectCategory.appendChild(option);
   });
 }
 
@@ -130,12 +143,15 @@ function deleteWork(workId) {
         console.error(`Erreur HTTP: ${response.status}`);
       } else {
         console.log('Travail supprimé avec succès');
+        // Mettre à jour la liste des travaux après la suppression réussie
+        allWorks = allWorks.filter(work => work.id !== workId);
       }
     })
     .catch(error => {
       console.error('Erreur :', error);
     });
 }
+
 
 let modal = null
 
@@ -213,3 +229,98 @@ document.getElementById('modal-add-btn').addEventListener('click', function() {
 document.getElementById('modal-close-btn').addEventListener('click', function() {
   document.getElementById('modal-add').style.display = "none";
 });
+
+
+//---------------------------------------------
+
+// Fonction pour gérer le soumission du formulaire
+function handleFormSubmit(e) {
+  e.preventDefault();
+
+  // Récupérer les valeurs du formulaire
+  const titleInput = document.getElementById('upload-title');
+  const imageInput = document.getElementById('add-work-btn');
+  const categorySelect = document.getElementById('select-category');
+
+  const title = titleInput.value;
+  const imageFile = imageInput.files[0];
+  const category = categorySelect.value;
+
+  // Validation des données du formulaire
+  if (!title || !imageFile || !category) {
+    alert("Veuillez remplir tous les champs du formulaire.");
+    return;
+  }
+
+  // Préparation des données du formulaire pour l'envoi
+  const formData = new FormData();
+  formData.append('title', title);
+  formData.append('image', imageFile);
+  formData.append('category', category);
+
+  // Envoi de la demande à l'API
+  fetch('http://localhost:5678/api/works', {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${localStorage.getItem('authToken')}`
+    },
+    body: formData
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Réponse de l’API:', data);
+      loadWorks(); // Recharger les travaux après l'ajout d'un nouveau
+      document.getElementById('modal-add').style.display = "none"; // Fermer la fenêtre modale après l'envoi du formulaire
+    })
+    .catch(error => console.error('Erreur:', error));
+}
+
+// Fonction pour gérer le changement d'entrée de fichier
+function handleFileChange() {
+  const fileInput = document.getElementById('add-work-btn');
+  const file = fileInput.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      // Modifier le contenu de la div pour montrer l'image
+      const imgPreview = document.getElementById('upload-img-preview');
+      imgPreview.innerHTML = `<img class="upload-img" src="${e.target.result}" alt="Image ajoutée">`;
+    };
+    reader.readAsDataURL(file);
+  }
+}
+
+// Fonction pour réinitialiser la div de prévisualisation
+function resetUploadPreview() {
+  const imgPreview = document.getElementById('upload-img-preview');
+  imgPreview.innerHTML = `
+    <img class="upload-logo" src="assets/images/Group.svg" alt="Ajouter une photo">
+    <label>+ Ajouter photo
+      <input id="add-work-btn" type="file" class="add-btn" accept=".jpg, .jpeg, .png" name="image"/>
+    </label>
+    <p>jpg, png : 4mo max</p>
+  `;
+
+  // Réajouter l'écouteur d'événements au bouton de téléchargement
+  const addWorkBtn = document.getElementById('add-work-btn');
+  if (addWorkBtn) {
+    addWorkBtn.addEventListener('change', handleFileChange);
+  }
+}
+
+// Ajouter l'écouteur d'événements au bouton de validation du formulaire
+const validationBtn = document.getElementById('validation-btn');
+validationBtn.addEventListener('click', handleFormSubmit);
+
+// Ajouter l'écouteur d'événements au bouton de téléchargement
+const addWorkBtn = document.getElementById('add-work-btn');
+if (addWorkBtn) {
+  addWorkBtn.addEventListener('change', handleFileChange);
+}
+
+// Ajouter les écouteurs d'événements pour réinitialiser la prévisualisation
+const modalCloseBtn = document.getElementById('modal-close-btn');
+modalCloseBtn.addEventListener('click', resetUploadPreview);
+
+const modalCloseArrow = document.querySelector('.js-modal-close');
+modalCloseArrow.addEventListener('click', resetUploadPreview);
