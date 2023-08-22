3f622d1ea86202a1f5e16490778293dc3c4ec2fc
jawadboughalem <jawadboughalem91@gmail.com>
2023-08-22 12:14:41 +0200

    Dernière modifications gestion de la réponse API

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -5,17 +5,21 @@ function getFormValues() {
   return { email, password };
 }
 
-// Fonction pour envoyer les informations de connexion à l'API
 function sendLoginInfo(loginData) {
   return fetch('http://localhost:5678/api/users/login', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(loginData)
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(loginData)
   })
-  .then(response => response.json());
+  .then(response => {
+    if (response.status === 404) {
+      return { error: 'email_not_found' };
+    }
+    return response.json();
+  });
 }
 
-// Fonction pour gérer la réponse de l'API
+
 function handleApiResponse(data, errorMsg) {
   if (data.error) {
     errorMsg.textContent = "Oups, votre identifiant ou votre mot de passe semble incorrect !";
@@ -27,23 +31,23 @@ function handleApiResponse(data, errorMsg) {
 
 
 
+
 document.addEventListener('DOMContentLoaded', function() {
   const form = document.querySelector('#login-form');
   const errorMsg = document.querySelector('.errorMsg');
 
   form.addEventListener('submit', function(e) {
-      e.preventDefault();
-
-      const loginData = getFormValues();
-      sendLoginInfo(loginData)
-      .then((data) => {
-        handleApiResponse(data, errorMsg)
-      })
-    
-      .catch(err => {
-          errorMsg.textContent = "Erreur lors de la connexion. Veuillez réessayer plus tard.";
-          console.error('Erreur:', err);
-      });
+    e.preventDefault();
+    const loginData = getFormValues();
+    sendLoginInfo(loginData)
+    .then((data) => {
+      handleApiResponse(data, errorMsg);
+    })
+    .catch(err => {
+      errorMsg.textContent = "Erreur lors de la connexion. Veuillez réessayer plus tard.";
+      console.error('Erreur:', err);
+    });
   });
+  
 });
 
